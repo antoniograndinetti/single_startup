055c240e27ea8c37f770327a333db98b759ce312
Antonio Grandinetti <antoniograndinetti91@gmail.com>
2016-06-16 15:13:52 +0200

    merged

--- server.py
+++ server.py
@@ -10,12 +10,11 @@ from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
-#import sendgrid
+# import sendgrid
+# from sendgrid.helpers.mail import *
 
 # make a secure connection to SendGrid
-# SENDGRID_API_KEY = 'your-sendgrid-api-key'
-# SENDGRID_DOMAIN = 'your-sendgrid-domain'
-# sg = sendgrid.SendGridClient(SENDGRID_API_KEY)
+#sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -59,26 +58,40 @@ class MainPage(webapp2.RequestHandler):
         result = FBUser.query(FBUser.fbid == self.request.get('fbID'))
 
         if result.fetch() == []:
-            if self.request.get('fbID') != None:
+            if self.request.get('fbID') != "":
                 fbuser_add = FBUser()
                 fbuser_add.fbid = self.request.get('fbID')
                 fbuser_add.nome = self.request.get('name')
                 fbuser_add.put()
 
 
-class AdminPage(webapp2.RequestHandler):
-    def get(self):
-        template = JINJA_ENVIRONMENT.get_template('admin.html')
-        self.response.write(template.render())
-    
-    def post(self):
-        username = self.request.get('username')
-        password = self.request.get('password')
+# class AdminPage(webapp2.RequestHandler):
+#     def get(self):
+#         send_message("antoniograndinetti91@gmail.com")
+#         client = sendgrid.SendGridClient(SENDGRID_API_KEY)
+#         message = sendgrid.Mail()
 
-        if (username == 'admin' and password == 'password'):
-            self.redirect('/confirm', {'user_param': username})
-        else:
-            self.redirect('/admin')
+#         message.add_to("antoniograndinetti91@gmail.com")
+#         message.set_from("noreply@singlestartupmarket.eu")
+#         message.set_subject("Sending with SendGrid is Fun")
+#         message.set_html("and easy to do anywhere, even with Python")
+
+#         client.send(message)
+#         template = JINJA_ENVIRONMENT.get_template('admin.html')
+#         self.response.write(template.render())
+    
+#     def post(self):
+#         username = self.request.get('username')
+#         password = self.request.get('password')
+#         if username != "":
+#             self.response.write("non vuoto")
+#         else:
+#             self.response.write("vuoto")
+
+        # if (username == 'admin' and password == 'password'):
+        #     self.redirect('/confirm', {'user_param': username})
+        # else:
+        #     self.redirect('/admin')
         
 
 
@@ -153,8 +166,8 @@ class FirmaStartup(webapp2.RequestHandler):
 
         # # invio delle mail
         # (status, msg) = send_message(self.request.get('email'))
-        # # inserire contatto admin
-        # (status, msg) = send_message(admin_mail)
+        # inserire contatto admin
+        # (status, msg) = send_message("antoniograndinetti91@gmail.com")
 
         template = JINJA_ENVIRONMENT.get_template('redirect.html')
         self.response.write(template.render())
@@ -162,17 +175,23 @@ class FirmaStartup(webapp2.RequestHandler):
 
 # funzione per inviare la mail
 # def send_message(destinatario):
-#     # [START sendgrid-send]
-#     message = sendgrid.Mail()
-#     message.set_subject('message subject')
-#     message.set_html('<strong>HTML message body</strong>')
-#     message.set_text('plaintext message body')
-#     message.set_from('Example App Engine Sender <sendgrid@{}>'.format(
-#         SENDGRID_DOMAIN))
-#     message.add_to(destinatario)
-#     status, msg = sg.send(message)
-#     return (status, msg)
-  
+# #     # [START sendgrid-send]
+# #     message = sendgrid.Mail()
+# #     message.set_subject('message subject')
+# #     message.set_html('<strong>HTML message body</strong>')
+# #     message.set_text('plaintext message body')
+# #     message.set_from('Example App Engine Sender <noreply@{}>'.format(
+# #         SENDGRID_DOMAIN))
+# #     message.add_to(destinatario)
+# #     status, msg = sg.send(message)
+# #     return (status, msg)
+#     from_email = Email("noreply@singlestartupmarket.eu")
+#     subject = "Hello World from the SendGrid Python Library"
+#     to_email = Email(destinatario)
+#     content = Content("text/plain", "some text here")
+#     mail = Mail(from_email, subject, to_email, content)
+#     response = sg.client.mail.send.post(request_body=mail.get())
+
 
 class Image(webapp2.RequestHandler):
     def get(self):
@@ -199,7 +218,7 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/firma', FirmaStartup),
     ('/image', Image),
-    ('/admin', AdminPage),
+#    ('/admin', AdminPage),
     ('/confirm', ConfirmPage),
     ('/quote1', Quote1Page),
     ('/quote2', Quote2Page)
